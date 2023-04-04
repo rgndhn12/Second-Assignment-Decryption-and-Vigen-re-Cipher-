0a4e137d5237fc8a8209d966f1488efa94fcf8b7
Rgn <reginefaemdahan@iskolarngbayan.pup.edu.ph>
2023-04-04 14:42:05 +0800

    Create Decryption Program

---
+++ Second Program.py
@@ -0,0 +1,58 @@
+#Dahan, Regine Fae M. (BSCPE 1-5) Second Code
+
+def main():
+    import pyfiglet
+    import time
+
+    the_intro ='Starting...'
+    print(pyfiglet.figlet_format(the_intro,font="digital"))
+    time.sleep(3)
+    print('Hello! This program is entitled DECRYPTION! It will accept a\nstring as encrypted text, then the program will decrypt it.')
+    time.sleep(2)
+
+    now_begin ='Let\'s start'
+    print(pyfiglet.figlet_format(now_begin,font="bubble"))
+    time.sleep (2)
+    
+    user_input = input (' Type your string >>> ')
+    the_output = ''
+
+    for i in range (len(user_input)):
+
+        if user_input[i] == '*':
+            the_output += 'a'
+        
+        elif user_input[i] == '&':
+            the_output += 'e'
+
+        elif user_input[i] == '#':
+            the_output += 'i'
+        
+        elif user_input[i] == '+':
+            the_output += 'o'
+        
+        elif user_input[i] == '!':
+            the_output += 'u'
+        
+        else:
+            the_output += user_input[i]
+
+    time.sleep(1)
+    the_decryption = ' Decrypting the string...'
+    print(pyfiglet.figlet_format(the_decryption,font="digital"))
+    time.sleep(2)
+    print ('The string\'s decryption is >>>',the_output)
+    
+    time.sleep(2)
+    while True:
+        print('==========================================================================')
+        try_again = input (' PRESS 1 if you want to try again. PRESS 2 if otherwise. ')
+
+        if (try_again == '1'):
+            main()
+        elif (try_again == '2'):
+            print(' Thank you for using my program : ) ')
+            exit()
+        else:
+            print('Invalid.')
+main()
